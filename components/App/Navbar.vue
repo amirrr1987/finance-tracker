3aef48c32e006cdf190626467c6ff8a76d7e8fe4
Amir Maghami <Maghami.A1987@gmail.com>
2024-10-13 16:23:11 +0330

    :sparkles: add defaults

---
+++ components/App/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="py-4 sm:py-6 lg:py-8 shadow">
+    <UContainer class="flex justify-between items-center">
+      <NuxtLink to="/">Fiance Tracker</NuxtLink>
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+      />
+    </UContainer>
+  </nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <AppNavbar />
+  <main>
+    <slot />
+  </main>
+</template>
+<style>
+body {
+  font-family: "Inter";
+}
+</style>
