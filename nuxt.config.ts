7dc6cb959e10b9308e7b97920a5bdc8b6da8e2ba
Amir Maghami <Maghami.A1987@gmail.com>
2024-10-13 15:12:03 +0330

    :sparkles: add modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/image",
+    "@nuxtjs/color-mode",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "@nuxt/content",
+    "@nuxt/eslint"
+  ]
 })
