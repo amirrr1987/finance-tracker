cb0c677d327b5c0c74082ee72d80d0f5fabeeb32
Amir Maghami <Maghami.A1987@gmail.com>
2024-10-13 16:24:08 +0330

    :sparkles: add Home section

---
+++ constants/index.ts
@@ -0,0 +1,1 @@
+export const transActionViewOptions = ["Daily", "Monthly", "Yearly"];
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="py-4 sm:py-6 lg:py-8">
+    <UContainer :ui="{ base: 'flex justify-between' }">
+      <h1 class="text-4xl font-bold">Summery</h1>
+      <USelectMenu
+        v-model="selectedView"
+        :options="transActionViewOptions"
+        placeholder="Select View"
+      />
+    </UContainer>
+  </section>
+</template>
+<script setup lang="ts">
+import { transActionViewOptions } from "~/constants";
+const selectedView = ref(transActionViewOptions[1]);
+</script>
