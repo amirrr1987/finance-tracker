223edf6bc1095b96b481c8f6b66423dc42d3565a
Amir Maghami <Maghami.A1987@gmail.com>
2024-10-15 14:49:33 +0330

    Update [id].get.ts

--- server/api/v1/transaction/[id].get.ts
+++ server/api/v1/transaction/[id].get.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Transaction } from "~/types/transaction.model";
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
   // محیط‌های مختلف را بررسی کنید تا مطمئن شوید از مقادیر صحیح استفاده می‌کنید.
   const supabaseUrl = process.env.SUPABASE_URL;
   const supabaseKey = process.env.SUPABASE_KEY;
@@ -14,7 +14,10 @@ export default defineEventHandler(async () => {
   const supabase = createClient(supabaseUrl, supabaseKey);
 
   // درخواست برای دریافت داده‌ها
-  const { data, error } = await supabase.from("transactions").select();
+  const { data, error } = await supabase
+    .from("transactions")
+    .select()
+    .eq("id", event.context.params?.id).single()
 
   if (error) {
     console.error("Error fetching transactions:", error);
