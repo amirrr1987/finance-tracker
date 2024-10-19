a11182da2e3ccf9e63a663f3e970a349d69af2db
Amir Maghami <Maghami.A1987@gmail.com>
2024-10-19 13:46:13 +0330

    ✔ add vueuse

--- components/App/Navbar.vue
+++ components/App/Navbar.vue
@@ -1,11 +1,24 @@
 <template>
-  <nav class="py-2 sm:py-3 lg:py-4 shadow bg-primary text-white">
+  <nav
+    class="py-2 sm:py-3 lg:py-4 shadow bg-primary dark:bg-primary-800 text-white"
+  >
     <UContainer class="flex justify-between items-center">
       <NuxtLink to="/">Fiance Tracker</NuxtLink>
-      <UAvatar
-        src="https://avatars.githubusercontent.com/u/739984?v=4"
-        alt="Avatar"
-      />
+      <div class="flex gap-x-4">
+        <UAvatar
+          src="https://avatars.githubusercontent.com/u/739984?v=4"
+          alt="Avatar"
+        />
+        <ClientOnly>
+          <USelect
+            v-model="colorMode.preference"
+            :options="['light', 'dark']"
+          />
+        </ClientOnly>
+      </div>
     </UContainer>
   </nav>
 </template>
+<script setup lang="ts">
+const colorMode = useColorMode();
+</script>
