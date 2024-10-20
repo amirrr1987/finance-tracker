4d905fd861cd19944987c599d2c9514372b85a3a
Amir Maghami <Maghami.A1987@gmail.com>
2024-10-20 14:01:37 +0330

    Update List.vue

--- components/Trend/List.vue
+++ components/Trend/List.vue
@@ -8,34 +8,58 @@
       <Trend
         color="green"
         title="Income"
-        :amount="300"
+        :amount="inComesTotal"
         :last-amount="400"
         :loading="transactionStore.isLoading.getAll"
       />
       <Trend
         color="red"
         title="Expense"
-        :amount="9900"
+        :amount="expenseTotal"
         :last-amount="1"
         :loading="transactionStore.isLoading.getAll"
       />
       <Trend
         color="green"
         title="Income"
-        :amount="4000"
+        :amount="inComesTotal"
         :last-amount="3000"
         :loading="transactionStore.isLoading.getAll"
       />
       <Trend
         color="green"
         title="Income"
-        :amount="4000"
+        :amount="expenseTotal"
         :last-amount="3000"
         :loading="transactionStore.isLoading.getAll"
       />
     </UContainer>
   </section>
+  <section class="py-4 md:py-6 lg:py-8">
+    <UContainer class="text-center">
+      <h3 class="text-2xl font-extrabold mb-2">Transactions</h3>
+      <p class="text-gray-600 dark:text-gray-400">
+        You has {{ inComeCount }} Incomes and {{ expenseCount }} expenses this
+        period
+      </p>
+    </UContainer>
+  </section>
 </template>
 <script setup lang="ts">
 const transactionStore = useTransactionStore();
+
+const inComes = computed(() =>
+  transactionStore.transactions.filter((i) => i.type === "Income")
+);
+const inComeCount = computed(() => inComes.value.length);
+const inComesTotal = computed(() =>
+  inComes.value.reduce((sum, transaction) => sum + transaction.amount, 0)
+);
+const expenses = computed(() =>
+  transactionStore.transactions.filter((i) => i.type === "Expense")
+);
+const expenseCount = computed(() => expenses.value.length);
+const expenseTotal = computed(() =>
+  expenses.value.reduce((sum, transaction) => sum + transaction.amount, 0)
+);
 </script>
