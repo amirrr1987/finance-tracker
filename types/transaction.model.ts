08208d84afdcd1866f53c461704cc07980c1cdc8
Amir Maghami <Maghami.A1987@gmail.com>
2024-10-21 14:48:31 +0330

    Update index.ts

--- types/transaction.model.ts
+++ types/transaction.model.ts
@@ -1,4 +1,28 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 import type { z } from "zod";
-import type { transactionSchema } from "~/schema/transaction.schema";
+import type { schema } from "~/schema/transaction.schema";
 
-export type Transaction = z.input<typeof transactionSchema>;
+export namespace TransactionDTO {
+  export type Content = z.input<typeof schema>;
+
+  export namespace CreateOne {
+    export type Request = Omit<Content, "id">;
+    export type Response = Content;
+  }
+
+  export namespace GetAll {
+    export type Response = Content[];
+  }
+  export namespace GetOne {
+    export type Request = Content["id"];
+    export type Response = Content;
+  }
+  export namespace UpdateOne {
+    export type Request = Content;
+    export type Response = Content;
+  }
+  export namespace DeleteOne {
+    export type Request = Content["id"];
+    export type Response = Content;
+  }
+}
