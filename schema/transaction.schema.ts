23e3195a0d6b13586de32edd699e520951e718b5
Amir Maghami <Maghami.A1987@gmail.com>
2024-10-21 14:48:18 +0330

    Update transaction.schema.ts

--- schema/transaction.schema.ts
+++ schema/transaction.schema.ts
@@ -1,13 +1,15 @@
 import { z } from "zod";
 
-export const transactionSchema = z.object({
-  amount: z.number(),
+export const schema = z.object({
+  amount: z.number().positive("Amount needs to be more than 0"),
   type: z.string(),
   description: z.string(),
   category: z.string(),
   id: z.number(),
   createdAt: z.string(),
 });
-export const transactionSchemaOnCreate = transactionSchema.omit({ id: true, createdAt: true });
-
-
+export const schemaCreateOne = schema.omit({ id: true });
+export const schemaUpdateOne = schema;
+export const schemaGetOne = schema.pick({ id: true });
+export const schemaGetAll = z.array(schema);
+export const schemaDeleteById = schema.pick({ id: true });
