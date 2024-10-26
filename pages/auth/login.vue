520b07038b4cfe8a65904b94331c3f3fa3b7b5b9
Amir Maghami <Maghami.A1987@gmail.com>
2024-10-27 00:05:03 +0330

    Update login.vue

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,37 +1,45 @@
-<template>
-  <UCard v-if="!success" header="signin to finince tracker">
-    <UForm :schema="{}" :state="{}" :ui="cardUi">
-      <UFormGroup
-        label="Email"
-        name="email"
-        required
-        help="You will receive an email width the confirmation link"
-      >
-        <UInput type="email" placeholder="Enter email" required />
-      </UFormGroup>
-      <UFormGroup label="Password" name="password" required>
-        <UInput type="password" placeholder="Enter password" required />
-      </UFormGroup>
-      <UButton type="submit" label="Login" @click="success = true" />
-    </UForm>
-  </UCard>
-  <UCard v-else header="Email has been send.."  :ui="cardUi">
-    <p>
-      We have send an email to <strong>mas@mm.com</strong>width a link to
-      signing
-    </p>
-    <p>Important the link has been expired</p>
-  </UCard>
-</template>
 <script setup lang="ts">
+import { z } from "zod";
+import type { FormSubmitEvent } from "#ui/types";
+import { authSchema } from "~/schema/auth.schema";
+import type { AuthDTO } from "~/types/auth.model";
+const schema = z.object({
+  email: authSchema.login.request,
+  password: z.string().min(4, "Must be at least 4 characters"),
+});
+
+type Schema = {
+  email: AuthDTO.Login.Request;
+};
+
+const state = reactive({
+  email: undefined,
+  password: undefined,
+});
+const auth = useAuth();
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  // Do something with data
+  console.log(event.data);
+  auth.email.value = state.email ?? "";
+  await auth.submit();
+}
 definePageMeta({
   layout: "auth",
 });
-const success = ref<boolean>(false);
-
-const cardUi = {
-  body: {
-    base: "text-center",
-  },
-};
 </script>
+
+<template>
+  <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+    <UFormGroup label="Email" name="email">
+      <UInput v-model="state.email" />
+    </UFormGroup>
+
+    <UFormGroup label="Password" name="password">
+      <UInput v-model="state.password" type="password" />
+    </UFormGroup>
+
+    <UButton type="submit" :loading="auth.pending.value === 'pending'">
+      Submit
+    </UButton>
+  </UForm>
+</template>
