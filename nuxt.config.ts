5106a3087d96f6f5fece5df30e4d132c833d5560
Amir Maghami <Maghami.A1987@gmail.com>
2024-10-27 16:08:54 +0330

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
     redirect: false,
     redirectOptions: {
       login: "/auth/login",
-      callback: "/",
+      callback: "/auth/confirm",
     },
   },
   dayjs: {
