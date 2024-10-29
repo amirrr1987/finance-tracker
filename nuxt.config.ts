f45edce9367e166f1607f156761534793bf54938
Amir Maghami <Maghami.A1987@gmail.com>
2024-10-29 16:29:44 +0330

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   ],
   plugins: ["~/plugins/logger"],
   supabase: {
-    redirect: false,
+    redirect: true,
     redirectOptions: {
       login: "/auth/login",
       callback: "/auth/confirm",
