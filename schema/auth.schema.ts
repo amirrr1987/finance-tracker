53ec7e80ce1002309cdec600a4aa28c22f7c3931
Amir Maghami <Maghami.A1987@gmail.com>
2024-11-03 17:20:20 +0330

    Update auth.schema.ts

--- schema/auth.schema.ts
+++ schema/auth.schema.ts
@@ -2,10 +2,14 @@ import { z } from "zod";
 
 export const schema = z.object({
   email: z.string().email(),
+  name: z.string().min(3),
 });
 export const authSchema = {
   content: schema,
   login: {
-    request: schema.shape.email
+    request: schema.shape.email,
+  },
+  save: {
+    request: schema
   }
-}
+};
