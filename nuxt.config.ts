942bdf6a82e34328e5ee500c975f9231336ea291
Amir Maghami <Maghami.A1987@gmail.com>
2024-11-05 08:57:05 +0330

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,6 @@ export default defineNuxtConfig({
     public: {
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseKey: process.env.SUPABASE_KEY,
-      baseUrl: process.env.BASE_URL ?? "http://localhost:3000",
     },
   },
 });
